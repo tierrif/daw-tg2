d57c231b2dc577ba97caf0b1b74e8cbdd365cbb5
Tierri Ferreira <tierriff@gmail.com>
2024-01-25 22:18:37 +0000

    Add bootstrap package.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,11 +6,20 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    @vite(['resources/js/app.js', 'resources/scss/app.scss'])
 </head>
 
 <body>
+    <nav class="navbar bg-primary" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand">PVML</a>
+            <ul class="navbar-nav mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="#scrollspyHeading1">Login</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
     @if ($page === 'home')
         <x-home />
     @elseif ($page === 'login')
