166c0d4c0101ae8e88115182c06ba7af7d189b28
Tierri Ferreira <tierriff@gmail.com>
2024-01-25 18:08:08 +0000

    Add basic component functionality

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,4 @@
+<!-- Main file for all pages. -->
 <!doctype html>
 <html lang="en">
 
@@ -10,7 +11,16 @@
 </head>
 
 <body>
-    <h1>Hello, world!</h1>
+    @if ($page === 'home')
+        <x-home />
+    @elseif ($page === 'login')
+        <x-login />
+    @elseif ($page === 'frequentuser')
+        <x-frequent-user />
+    @else
+        <x-not-found />
+    @endif
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
